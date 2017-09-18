2d2872acd0c1a42ed80860c5c1db06a7fc2a3d77
Vladislav <druzhbinsky@institute-bm.ru>
2017-09-18 18:58:07 +0300

    statret-kit 1.0.1

--- src/app.js
+++ src/app.js
@@ -1,8 +1,21 @@
 import React, { Component } from "react"
+import {Provider} from "react-redux"
+import {createStore} from "redux"
+
+const userState = ["one", "two"]
+const reducer = (state = userState, action) => {
+	return state
+}
+const store = createStore(reducer)
+window.store = store
 
 class App extends Component {
 	render() {
-		return <div>Text3</div>
+		return (
+			<Provider store={store}>
+				<div>Hello</div>
+			</Provider>
+		)
 	}
 }
 
