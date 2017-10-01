49a41a8b2b5811adaa4448b0b434a4261816e380
Vladislav <druzhbinsky@institute-bm.ru>
2017-10-01 16:42:21 +0300

    middlewares

--- src/redux/store/store.js
+++ src/redux/store/store.js
@@ -1,6 +1,9 @@
 import {createStore, applyMiddleware} from "redux"
-import reduxThunk from "redux-thunk"
 import {composeWithDevTools} from "redux-devtools-extension"
 
+//middlewars
+import reduxThunk from "redux-thunk"
+import logger from "../middlewars/logger"
+
 import rootReducer from "../reducers/rootReducer"
-export const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(reduxThunk)))
+export const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(reduxThunk, logger)))
